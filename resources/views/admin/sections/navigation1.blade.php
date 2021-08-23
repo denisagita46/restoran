69c3fe181b0e4c77eb19375e74e1a7d1d86095d0
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-23 14:47:53 +0700

    Add files via upload

---
+++ resources/views/admin/sections/navigation1.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+        <div class="navbar nav_title" style="border: 0;">
+            <a href="/home" class="site_title"></i><span>Aplikasi Restoran</span></a>
+        </div>
+
+        
+    </div>
+</div>
