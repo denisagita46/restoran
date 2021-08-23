1649649ade449a2800aaa5bdd38103e4e69fe8fa
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-23 14:34:10 +0700

    kitchendashboard

---
+++ app/Http/Controllers/Web/DashbordKitchen.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashbordKitchen extends Controller
+{
+    //
+	 public function index() {
+		
+		return view('dahsboard_kitchen.dashboard');
+
+    }
+}
