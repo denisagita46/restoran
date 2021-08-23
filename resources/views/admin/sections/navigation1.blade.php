ee93d18890b328b6ca52e0dee3e3621e52a14fc4
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-23 15:00:06 +0700

    Update navigation1.blade.php

--- resources/views/admin/sections/navigation1.blade.php
+++ resources/views/admin/sections/navigation1.blade.php
@@ -1,9 +1,22 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
-            <a href="/home" class="site_title"></i><span>Aplikasi Restoran</span></a>
+            <a href="/home" class="site_title"></i><span>Aplkasi Restoran</span></a>
         </div>
 
-        
+        <div class="clearfix"></div>
+
+        <div class="profile clearfix">
+            <div class="profile_pic">
+				<img src="{{ asset('images/img.jpg') }}" alt="..." class="img-circle profile_img">    
+            </div>
+			
+            <div class="profile_info">
+                <span>Welcome,</span>
+				{{ session('flag=1') }}
+				<h2>{{ auth()->user()->name }}</h2>
+               
+            </div>
+        </div>
     </div>
 </div>
