0468cd72e3ed237c219f991f54d1eccefbdda67e
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-24 09:21:59 +0700

    Update add_menu.blade.php

--- resources/views/menu/add_menu.blade.php
+++ resources/views/menu/add_menu.blade.php
@@ -81,8 +81,8 @@
                     @csrf
                     <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-
                             <button type="submit" class="btn btn-success">Create</button>
+			    <a href="{{ route('menu') }}" class="btn btn-success">Kembali</a>
                         </div>
                     </div>
                 </form>
