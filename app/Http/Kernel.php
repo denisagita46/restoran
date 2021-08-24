8d1a8ac7d22156b4a3acac021537cfe4e2c68b89
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-24 13:42:57 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-		'kasir' => \App\Http\Middleware\AdminMiddleware::class,
+	'kasir' => \App\Http\Middleware\AdminMiddleware::class,
         'kitchen' => \App\Http\Middleware\UserMiddleware::class,
+	'revalidate' => \App\Http\Middleware\RevalidateBackHistory::class,
     ];
 }
