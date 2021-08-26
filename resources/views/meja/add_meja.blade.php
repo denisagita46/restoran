ffa92adebd26a2e905814ae63962ef7a9d17b3f9
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-27 06:57:49 +0700

    Update add_meja.blade.php

--- resources/views/meja/add_meja.blade.php
+++ resources/views/meja/add_meja.blade.php
@@ -50,8 +50,8 @@
                     @csrf
                     <div class="form-group">
                         <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-
                             <button type="submit" class="btn btn-success">Create</button>
+			    <a href="{{ route('meja') }}" class="btn btn-primary">Kembali</a>
                         </div>
                     </div>
                 </form>
