1a0839ddb123d80be4490521b68d95f6ada17eb9
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-27 06:39:03 +0700

    Update table_meja.php

--- app/Models/table_meja.php
+++ app/Models/table_meja.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class table_meja extends Model
 {
-    //
+    protected $table = 'table_meja';
+    protected $primaryKey = 'id_meja';
+    protected $fillable = [
+        'id_meja', 'nomor_meja','lokasi'
+    ];
 }
