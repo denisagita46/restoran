8635077e2ab60319c09845ecaf4eb1c5323e7abb
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-27 06:46:51 +0700

    model table menu

--- app/Models/table_menu.php
+++ app/Models/table_menu.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class table_menu extends Model
 {
     //
-	 protected $fillable = [
+    protected $table = 'table_menu';
+    protected $primaryKey = 'id_menu';
+    protected $fillable = [
         'id_menu', 'kategori_menu','nama_menu','harga','deskripsi'
     ];
 }
