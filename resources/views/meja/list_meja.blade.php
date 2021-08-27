e61be1902220ce5db22cffbaa4053d762d2164f6
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-27 22:49:49 +0700

    detail view

--- resources/views/meja/list_meja.blade.php
+++ resources/views/meja/list_meja.blade.php
@@ -50,7 +50,7 @@
                                         <td>{{ $table_meja->lokasi }}</td>
 					<td>
 					<center> 
-						<a href="" class="btn btn-primary"><span class="fa fa-check"> View |</a>
+						<a href="/view/{{ $table_meja->id_meja }}" class="btn btn-primary"><span class="fa fa-check"> View |</a>
 						<a href="/edit/{{ $table_meja->id_meja }}" class="btn btn-warning"><span class="fa fa-edit"> Edit</a>
 						<a href="/hapus_meja/{{ $table_meja->id_meja }}"  onclick="return confirm('Yakin mau hapus data ini?')" class="btn btn-danger"><span class="fa fa-trash"> Delete |</a>
 					</center></td>
