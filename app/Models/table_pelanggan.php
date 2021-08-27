6accb5f17d580a2c68952b0d88b260b4ae28f984
deni sagita <51990684+denisagita46@users.noreply.github.com>
2021-08-27 22:56:35 +0700

    edit pelanggan

--- app/Models/table_pelanggan.php
+++ app/Models/table_pelanggan.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class table_pelanggan extends Model
 {
-    //
+	protected $table = 'table_pelanggan';
+        protected $primaryKey = 'id_pelanggan';
 	protected $fillable = [
         'id_pelanggan', 'nama_pelanggan','telepon','jenis_kelamin','email','alamat'
     ];
